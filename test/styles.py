cbad752aee2676b7568d6b6d7ed30de7e70f9725
angelCarias <69056344+angelCarias@users.noreply.github.com>
2020-09-15 18:39:48 -0600

    Italic

--- test/styles.py
+++ test/styles.py
@@ -5,9 +5,17 @@ class Bold:
     def __repr__(self):
         return f"**{self.text}**"
 
+class Italic:
+    def __init__(self, text: str):
+        self.text = text
+
+    def __repr__(self):
+        return f"*{self.text}*"
+
 class Strikeout:
     def __init__(self, text: str):
         self.text = text
 
     def __repr__(self):
         return f"~~{self.text}~~"
+
