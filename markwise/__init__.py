60bdc2fd0ca7c600026c62e75a5ac6ba9ca44b1b
angelCarias <69056344+angelCarias@users.noreply.github.com>
2020-09-16 07:40:55 -0600

    Update build data

--- markwise/__init__.py
+++ markwise/__init__.py
@@ -1,7 +1,7 @@
 from .obj import *
 from .widgets import *
 
-__title__ = 'color_palette'
-__author__ = 'SnowballSH, 12944qwerty, one-wq'
+__title__ = 'markwise'
+__author__ = 'SnowballSH, Tekgar'
 __license__ = 'MIT'
-__version__ = '0.2.5'
+__version__ = '0.1.0'
