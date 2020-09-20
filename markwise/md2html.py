123883148a4db006909a5660872521423a763a80
angelCarias <69056344+angelCarias@users.noreply.github.com>
2020-09-19 19:27:30 -0600

    Structure change

--- markwise/md2html.py
+++ markwise/md2html.py
@@ -1,21 +1,20 @@
 from markdown import markdown as mdconv
 from .obj import MarkDown
 
-
-def md2html(md: MarkDown, file=None):
-    ht = mdconv(md.text)
-    if file is not None:
-        with open(file, "w") as f:
-            f.write(ht)
-
-    return ht
-
-
-def md2html_f(md, file=None):
-    with open(md, "r") as f:
-        ht = mdconv(f)
-    if file is not None:
-        with open(file, "w") as f:
-            f.write(ht)
-
-    return ht
+class Conversion:
+    def md2html(md: MarkDown, file=None):
+        ht = mdconv(md.text)
+        
+        if file is not None:
+            with open(file, "w") as f:
+                f.write(ht)
+        return ht
+    
+    def md2html_f(md, file=None):
+        with open(md, "r") as f:
+            ht = mdconv(f)
+            
+            if file is not None:
+                with open(file, "w") as f:
+                    f.write(ht)
+       return ht
